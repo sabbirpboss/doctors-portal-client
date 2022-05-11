fd7e0820d24f1cd10c7d6a623a07e341ac090185
sabbirpboss <sabbir.ah799@gmail.com>
2022-05-11 11:17:31 +0600

    initial commit and project setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className='text-5xl'>Hello world</h1>
+      <button className="btn px-16">Hit Me</button>
+      <button class="btn btn-primary rounded-full w-64">Button</button>
+      <button className="btn btn-accent btn-outline">Hire Me</button>
     </div>
   );
 }
