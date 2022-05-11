8b9628be3b3ca1d1104413cc4f74105ab43eba7f
sabbirpboss <sabbir.ah799@gmail.com>
2022-05-12 00:27:38 +0600

    banner bg added successfully

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        "nature-light": "url('/src/assets/images/bg.png')",
+      },
+    },
   },
   plugins: [require("daisyui")],
   daisyui: {
