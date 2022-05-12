dcbd6eba665d3adf05478dc9d590d3e005a989af
sabbirpboss <sabbir.ah799@gmail.com>
2022-05-12 13:41:36 +0600

    services done

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -5,7 +5,7 @@ const Banner = () => {
       <div className="hero-pattern bg-nature-light">
       <div class="hero min-h-screen">
         <div class="hero-content flex-col lg:flex-row-reverse">
-          <img src={chair} class="max-w-sm rounded-lg shadow-2xl" alt="Banner" />
+          <img src={chair} class="sm:max-w-sm lg:max-w-lg xl:max-w-xl 2xl:max-w-lg rounded-lg shadow-2xl ml-4" alt="Banner" />
           <div>
             <h1 class="text-5xl font-bold">Your New Smile Starts Here</h1>
             <p class="py-6">
--- src/Pages/Home/Services/Services.js
+++ src/Pages/Home/Services/Services.js
@@ -3,6 +3,7 @@ import fluoride from "../../../assets/images/fluoride.png";
 import cavity from "../../../assets/images/cavity.png";
 import whitening from "../../../assets/images/whitening.png";
 import Service from "./Service/Service";
+import treatment from "../../../assets/images/treatment.png";
 
 const Services = () => {
   const services = [
@@ -39,8 +40,26 @@ const Services = () => {
       </div>
       <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3">
         {services.map((service) => (
-          <Service key={service._id} service={service} imgSWidth="w-28 h-28"></Service>
+          <Service
+            key={service._id}
+            service={service}
+            imgSWidth="w-28 h-28"
+          ></Service>
         ))}
+      </div>
+      <div class="hero min-h-screen bg-base-100 bg-nature-light py-12">
+        <div class="hero-content flex-col lg:flex-row">
+          <img src={treatment} class="max-w-sm rounded-lg shadow-2xl" />
+          <div className="px-12">
+            <h1 class="text-5xl font-bold">Exceptional Dental Care, on Your Terms</h1>
+            <p class="py-6">
+            It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsumis that it has a more-or-less normal distribution of letters,as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page
+            </p>
+            <button class="btn btn-secondary text-white font-bold bg-gradient-to-r from-secondary to-primary">
+              Get Started
+            </button>
+          </div>
+        </div>
       </div>
     </div>
   );
